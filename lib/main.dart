95c487588d128c95f74f1fd313709a610c713ab8
Anderson Coimbra <andersonmegax@hotmail.com>
2023-10-05 00:26:16 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,190 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'IMC',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a blue toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  TextEditingController alturaController = TextEditingController();
+  TextEditingController pesoController = TextEditingController();
+  String _imc = "";
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  void _calculaIMC() {
+    double altura = double.parse(alturaController.text) / 100;
+    double peso = double.parse(pesoController.text);
+    double imc = peso / (altura * altura);
+    setState(() {
+      if (imc < 18.5) {
+        _imc =
+            "Seu IMC é: ${imc.toStringAsPrecision(3)} classificado como: Abaixo do peso";
+      } else if (imc >= 18.5 && imc < 25) {
+        _imc =
+            "Seu IMC é: ${imc.toStringAsPrecision(3)} classificado como: Peso normal";
+      } else if (imc >= 25 && imc < 30) {
+        _imc =
+            "Seu IMC é: ${imc.toStringAsPrecision(3)} classificado como: Sobrepeso";
+      } else if (imc >= 30 && imc < 35) {
+        _imc =
+            "Seu IMC é: ${imc.toStringAsPrecision(3)} classificado como: Obesidade grau 1";
+      } else if (imc >= 35 && imc < 40) {
+        _imc =
+            "Seu IMC é: ${imc.toStringAsPrecision(3)} classificado como: Obesidade grau 2";
+      } else if (imc >= 40) {
+        _imc =
+            "Seu IMC é: ${imc.toStringAsPrecision(3)} classificado como: Obesidade grau 3";
+      } else {
+        _imc =
+            "Seu IMC é: ${imc.toStringAsPrecision(3)} classificado como: Não foi possível calcular";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextFormField(
+              controller: alturaController,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Altura',
+                hintText: "Digite dua altura em centimetros",
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Por favor, digite sua altura';
+                }
+                if (int.parse(value) < 40 || int.parse(value) > 250) {
+                  return 'Por favor, digite uma altura válida';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: pesoController,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Peso',
+                hintText: "Digite seu peso em quilogramas",
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Por favor, digite seu peso';
+                }
+                if (int.parse(value) < 1 || int.parse(value) > 500) {
+                  return 'Por favor, digite um peso válido';
+                }
+                return null;
+              },
+            ),
+            Text(
+              _imc,
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _calculaIMC,
+        child: const Text("IMC"),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
